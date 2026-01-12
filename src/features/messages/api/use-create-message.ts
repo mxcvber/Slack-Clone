79e4020ec45607d2bbec7cd9b88a1dfad0ff11eb
max <mxdigitvl@gmail.com>
2026-01-12 20:15:04 +0200

    Schema finalization

--- src/features/messages/api/use-create-message.ts
+++ src/features/messages/api/use-create-message.ts
@@ -9,6 +9,7 @@ type RequestType = {
   image?: Id<'_storage'>
   channelId?: Id<'channels'>
   parentMessage?: Id<'messages'>
+  conversationId?: Id<'conversations'>
 }
 type ResponseType = Id<'messages'> | null
 
